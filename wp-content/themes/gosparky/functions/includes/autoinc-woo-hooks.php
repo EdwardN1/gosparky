8b1f1166ecf9e2bc7c44c472f9b1d4a9181b980a
Edward Nickerson <edward@technicks.com>
2021-06-22 17:38:36 +0100

    Day 2 Go Sparky

--- wp-content/themes/gosparky/functions/includes/autoinc-woo-hooks.php
+++ wp-content/themes/gosparky/functions/includes/autoinc-woo-hooks.php
@@ -57,23 +57,78 @@ function woocommerce_template_single_price_override()
     <?php
 }
 
-add_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 50);
+/*add_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 50);
 add_action('woocommerce_before_add_to_cart_quantity', 'woocommerce_before_add_to_cart_quantity_minus_btn', 10);
-add_action('woocommerce_after_add_to_cart_quantity', 'woocommerce_before_add_to_cart_quantity_plus_btn', 10);
+add_action('woocommerce_after_add_to_cart_quantity', 'woocommerce_before_add_to_cart_quantity_plus_btn', 20);
 
 function woocommerce_before_add_to_cart_quantity_minus_btn()
 {
-    ?>
-    <button class="btn minus1">-</button>
+    */ ?><!--
+    <button class="btn minus1" style="font-size: 26px">-</button>
     <?php
-}
+/*}
 
 function woocommerce_before_add_to_cart_quantity_plus_btn()
 {
-    ?>
-    <button class="btn plus1">+</button>
-    <?php
+    */ ?>
+    <button class="btn plus1" style="font-size: 26px">+</button>
+    --><?php
+/*
+}*/
 
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart_override', 50);
+
+function woocommerce_template_single_add_to_cart_override()
+{
+    global $product;
+
+    if (!$product->is_purchasable()) {
+        return;
+    }
+
+    echo wc_get_stock_html($product); // WPCS: XSS ok.
+
+    if ($product->is_in_stock()) : ?>
+
+        <?php do_action('woocommerce_before_add_to_cart_form'); ?>
+
+        <form class="cart"
+              action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>"
+              method="post" enctype='multipart/form-data'>
+            <?php do_action('woocommerce_before_add_to_cart_button'); ?>
+            <div class="grid-x">
+                <div class="cell shrink">
+                    <button class="btn minus1" style="font-size: 26px">-</button>
+                </div>
+                <div class="cell shrink">
+                    <?php
+                    do_action('woocommerce_before_add_to_cart_quantity');
+
+                    woocommerce_quantity_input(
+                        array(
+                            'min_value' => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
+                            'max_value' => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
+                            'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+                        )
+                    );
+
+                    do_action('woocommerce_after_add_to_cart_quantity');
+                    ?>
+                </div>
+                <div class="cell shrink">
+                    <button class="btn plus1" style="font-size: 26px">+</button>
+                </div>
+                <div class="cell auto">
+                    <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>"
+                            class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
+                </div>
+            </div>
+            <?php do_action('woocommerce_after_add_to_cart_button'); ?>
+        </form>
+
+        <?php do_action('woocommerce_after_add_to_cart_form'); ?>
+
+    <?php endif;
 }
 
 add_action('woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 55);
